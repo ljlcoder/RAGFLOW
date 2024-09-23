89736c1c90f3c6940fd56281eeb30c171a0c277c
jason-qi <10402238+jason-qi@user.noreply.gitee.com>
2024-09-23 16:49:59 +0800

    privacy

--- llama3/email_send.py
+++ llama3/email_send.py
@@ -1,10 +1,10 @@
 import requests
 
-domain_name = "sandbox000e649976434b279945a954878ef16a.mailgun.org"
+domain_name = "xxx.mailgun.org"
 url = "https://api.mailgun.net/v3/" + domain_name + "/messages"
 
 data = {
-        "from": "mailgun@sandbox000e649976434b279945a954878ef16a.mailgun.org", #set your from mail address here
+        "from": "mailgun@xxx.mailgun.org", #set your from mail address here
         "to": "xxx@qq.com",
         "subject": '测试邮件',
         "text": '试一下能发过去吗',
@@ -13,7 +13,7 @@ data = {
 
 #headers = {"Content-Type": "multipart/form-data"}   headers=headers,
 
-response = requests.post(url,  auth=('api','45a854925b30fcf651de73bd15512ca1-2175ccc2-f4f07960'),data=data,)
+response = requests.post(url,  auth=('api','xxx-xxx-xxx'),data=data,)
 
 data = response.json()
 print(data)
